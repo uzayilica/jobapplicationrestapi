10d9bc81d30729f6d6b2f981b59672e5a4424eeb
uzayilica <uzayilica2@gmail.com>
2024-09-20 15:25:33 +0300

    .

---
+++ src/main/java/com/uzay/jobapplicationproject/JobApplicationProjectApplication.java
@@ -0,0 +1,20 @@
+package com.uzay.jobapplicationproject;
+
+import com.uzay.jobapplicationproject.entity.Company;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class JobApplicationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplicationProjectApplication.class, args);
+    }
+
+    }
+
+
